8a7ce8b76dc9d5bf3a478fbd22bbf4b1d412bdc4
Waqar Ahmed <waqar.17a@gmail.com>
2024-01-27 23:32:36 +0500

    Make the categories filterable

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,24 +12,74 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _filterText = ValueNotifier("");
+  var _isSearching = false;
+
+  List<String> _filteredCategories() {
+    String filterText = _filterText.value.toLowerCase();
+    return categories_en
+        .where((cat) => cat.toLowerCase().contains(filterText))
+        .toList();
+  }
+
+  Widget _buildAppbarTitleWidget() {
+    if (!_isSearching) {
+      return Text(widget.title);
+    }
+    return TextField(
+      autofocus: true,
+      decoration: const InputDecoration(
+        hintText: "Filter...",
+        border: InputBorder.none,
+      ),
+      onChanged: (query) => _filterText.value = query,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // BEGIN appbar
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: ListView.builder(
-        itemCount: categories_en.length,
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: ListTile(
-              title: Text(categories_en[index]),
-              onTap: () {
-                Navigator.of(context)
-                    .pushNamed(Routes.showCategory, arguments: index);
+        leading: _isSearching
+            ? CloseButton(onPressed: () {
+                setState(() {
+                  _isSearching = false;
+                });
+              })
+            : null,
+        title: _buildAppbarTitleWidget(),
+        actions: [
+          // Search button
+          if (_isSearching == false)
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                setState(() {
+                  _isSearching = true;
+                });
               },
-            ),
+            )
+        ],
+      ), // END appbar
+      body: ValueListenableBuilder(
+        valueListenable: _filterText,
+        builder: (context, value, _) {
+          final cateogries = _filteredCategories();
+          return ListView.builder(
+            itemCount: cateogries.length,
+            itemBuilder: (ctx, index) {
+              return Card(
+                child: ListTile(
+                  title: Text(cateogries[index]),
+                  onTap: () {
+                    Navigator.of(context)
+                        .pushNamed(Routes.showCategory, arguments: index);
+                  },
+                ),
+              );
+            },
           );
         },
       ),
